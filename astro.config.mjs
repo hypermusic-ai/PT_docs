5037e160a984e4ceeffa9a1ab2cae3aab3703afc
Adam Łukawski <63563505+sunsetsobserver@users.noreply.github.com>
2024-09-02 21:20:13 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Performative Transactions - Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/sunsetsobserver/PT_docs',
 			},
 			sidebar: [
 				{
